e1f6f051927523ffe56e428404441422ec8b4f40
Lucas Israel Laudimir Silva <lucasisrael85@gmail.com>
2023-08-30 10:58:10 -0300

    python_course

---
+++ app.py
@@ -0,0 +1,3 @@
+age, name = (28, 'Lucas')
+print(f'Meu nome é {name} e tenho {age} ano(s) de idade')
+
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = 'Lucas'
+idade = '28'
+
+nome = 'Samuel'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["MG", "SP", "RJ", "SC"]
+
+print(BRAZILIAN_STATES)
